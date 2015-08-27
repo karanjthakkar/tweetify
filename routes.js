a23ccfddb25698b53b9e8eba6c79a89456cf5d06
Karan Thakkar <karanjthakkar@gmail.com>
2015-08-28 01:10:17 +0530

    Add temp port number to check redirect

--- routes.js
+++ routes.js
@@ -36,7 +36,7 @@ module.exports = function(app, passport) {
       failureRedirect: '/'
     }),
     function(req, res) {
-      res.redirect('//tweetify.io/?code=' + req.user.id);
+      res.redirect('//tweetify.io:4200/?code=' + req.user.id);
     });
 
   app.post('/logout', function(req, res) {
