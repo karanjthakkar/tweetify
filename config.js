7327dc221feba11d49bea2d4700b8cfd224c0a59
Karan Thakkar <karanjthakkar@gmail.com>
2015-08-28 01:03:01 +0530

    Change callback url to point to live

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
   test: {
-    callbackURL: '//localhost:3000/auth/twitter/callback',
-    dbUrl: 'mongodb://localhost:27017/tweetlyapp',
+    callbackURL: '//api.tweetify.io/auth/twitter/callback',
+    dbUrl: 'mongodb://127.0.0.1:27017/tweetlyapp',
     TWITTER_CONSUMER_KEY: process.env.tweetly_consumer_key,
     TWITTER_CONSUMER_SECRET: process.env.tweetly_consumer_secret,
     TWITTER_ACCESS_TOKEN: process.env.tweetly_access_token,
